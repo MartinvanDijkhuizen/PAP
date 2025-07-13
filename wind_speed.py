4f6b2bc882a8d8fe5157c045b62b8a0847abaf78
MartinvanDijkhuizen <martin.vandijkhuizen@hu.nl>
2025-07-13 20:58:15 +0200

    new file:   wind_speed.csv (data of anemeter) 	new file:   wind_speed.py (adafruit anemometer measures wind speed)

---
+++ wind_speed.py
@@ -0,0 +1,58 @@
+from datetime import datetime
+import csv
+import time
+import board
+import busio
+import digitalio
+import adafruit_mcp3xxx.mcp3008 as MCP
+from adafruit_mcp3xxx.analog_in import AnalogIn
+import numpy
+
+# anemometer defaults
+anemometer_voltage    = numpy.array([0.404, 2.0])
+anemometer_wind_speed = numpy.array([0, 32.4])
+# a and b of lineair function y = ax + b, to calculate wind_speed
+a, b = numpy.polyfit(anemometer_voltage, anemometer_wind_speed, 1)
+
+# Initialize SPI connection
+spi = busio.SPI(clock = board.SCK, MISO = board.MISO, MOSI = board.MOSI)
+# Assign a chip select pin
+cs = digitalio.DigitalInOut(board.D7)
+# Create MCP3008 object
+mcp = MCP.MCP3008(spi, cs)
+# Create analog input chanel for anemometer on the MCP3008 pin 0
+channel = AnalogIn(mcp, MCP.P0)
+
+csv_file = "wind_speed.csv"
+
+# Define wind_speed function
+def calculate_wind_speed(x):
+	ws = round(a * x + b, 1)
+	return ws
+
+# Ensure CSV file has headers
+try:
+    with open(csv_file, "x", newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(["Tijdstip", "Windsnelheid"])
+except FileExistsError:
+    pass
+
+# Prevent all cacalations from taking place at the same time
+time.sleep(210)
+
+while True:
+	try:
+		timestamp = datetime.now().strftime("%d-%m-%Y %H:%M")
+		wind_speed = calculate_wind_speed(channel.voltage)
+		print(f"[{timestamp}] Windsnelheid: {wind_speed}m/s")
+		with open(csv_file, "a", newline='') as f:
+			writer = csv.writer(f)
+			writer.writerow([timestamp, wind_speed])
+	except Exception as e:
+		timestamp = datetime.now().strftime("%d-%m-%Y %H:%M")
+		with open("wind_errors.log", "a") as log:
+			log.write(f"[{timestamp}] Fout: {str(e)}\n")
+			print("Fout bij het lezen van de sensor:", e)
+
+	time.sleep(300) # every 5 minutes
